fca144cb9d3dffe335b49fbbab2c8973c5b999c7
Serhii Myshko <sergeiown@gmail.com>
2023-02-22 15:03:12 +0200

    make local version of the calendar

--- js/addBlankCells.js
+++ js/addBlankCells.js
@@ -2,7 +2,7 @@ export function addBlankCells(month, year, i, weekRow) {
     if (i === 1) {
         const firstDay = new Date(year, month, 1).getDay();
 
-        // if the first day of the month is not a Sunday add blank cells
+        // if the first day of the month is not a Monday add blank cells
         for (let j = 0; j < firstDay; j++) {
             const emptyCell = document.createElement('td');
             weekRow.appendChild(emptyCell);
--- js/markDays.js
+++ js/markDays.js
@@ -1,5 +1,5 @@
 export function markDays(month, year, i, dayCell) {
-    if (new Date(year, month, i).getDay() === 0 || new Date(year, month, i).getDay() === 6) {
+    if (new Date(year, month, i).getDay() === 5 || new Date(year, month, i).getDay() === 6) {
         // if the current day is a weekend - set background color to "weekend"
         dayCell.classList.add('weekend');
     } else if (new Date(year, month, i).toLocaleDateString() === new Date().toLocaleDateString()) {
