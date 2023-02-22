5c59c144b4680b5dbdc4ab973293a547be261814
Serhii Myshko <sergeiown@gmail.com>
2023-02-22 15:32:52 +0200

    fix "sunday" problem

--- js/addBlankCells.js
+++ js/addBlankCells.js
@@ -1,6 +1,9 @@
 export function addBlankCells(month, year, i, weekRow) {
     if (i === 1) {
-        const firstDay = new Date(year, month, 1).getDay();
+        let firstDay = new Date(year, month, 1).getDay();
+        if (firstDay === 0) {
+            firstDay = 7;
+        }
 
         // if the first day of the month is not a Monday add blank cells
         for (let j = 1; j < firstDay; j++) {
